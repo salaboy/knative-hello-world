a883ae1369f87c468c3a86236913138f70ad64d5
salaboy <salaboy@gmail.com>
2020-08-19 09:55:36 +0100

    updating to use new cloud events spec version

--- src/main/java/com/salaboy/knative/knativehelloworld/KnativeHelloWorldApplication.java
+++ src/main/java/com/salaboy/knative/knativehelloworld/KnativeHelloWorldApplication.java
@@ -1,21 +1,20 @@
 package com.salaboy.knative.knativehelloworld;
 
+
 import com.salaboy.cloudevents.helper.CloudEventsHelper;
 import io.cloudevents.CloudEvent;
-import io.cloudevents.v03.AttributesImpl;
+
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.util.MultiValueMap;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
-import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Mono;
 
-import java.util.Map;
 import java.util.logging.Logger;
 
 @SpringBootApplication
 @RestController
+@Slf4j
 public class KnativeHelloWorldApplication {
 
     public static void main(String[] args) {
@@ -34,10 +33,24 @@ public class KnativeHelloWorldApplication {
 
 
     @PostMapping
-    public String recieveCloudEvent(@RequestHeader Map<String, String> headers, @RequestBody Object body) {
-        CloudEvent<AttributesImpl, String> cloudEvent = CloudEventsHelper.parseFromRequest(headers, body);
+    public String recieveCloudEvent(@RequestHeader HttpHeaders headers, @RequestBody Object body) {
+        CloudEvent cloudEvent = CloudEventsHelper.parseFromRequest(headers, body);
         System.out.println("> I got a cloud event: " + cloudEvent.toString());
-        System.out.println("  -> cloud event attr: " + cloudEvent.getAttributes());
+        for(String key : cloudEvent.getAttributeNames())
+        System.out.println("  -> cloud event attr ("+key+"): " + cloudEvent.getAttribute(key));
+
+        System.out.println("  -> cloud event data: " + cloudEvent.getData());
+
+        return "OK!";
+    }
+
+    @PostMapping("/another")
+    public String recieveCloudEventAnother(@RequestHeader HttpHeaders headers, @RequestBody Object body) {
+        CloudEvent cloudEvent = CloudEventsHelper.parseFromRequest(headers, body);
+        System.out.println("> I got a cloud event: " + cloudEvent.toString());
+        for(String key : cloudEvent.getAttributeNames())
+            System.out.println("  -> cloud event attr ("+key+"): " + cloudEvent.getAttribute(key));
+
         System.out.println("  -> cloud event data: " + cloudEvent.getData());
 
         return "OK!";
